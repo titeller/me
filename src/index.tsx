1cb1d610b95e20dba85d3487df7734500dcf8f13
Ti Teller <kitti.piyapan@gmail.com>
2018-10-10 00:59:22 +0700

    add parcel scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<div>555</div>, document.getElementById('root'))
+ReactDOM.render(<div>555 wp</div>, document.getElementById('root'))
