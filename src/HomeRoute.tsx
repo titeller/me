c92026ac9130b623f929b7d65437f5674ca7d76a
Ti Teller <kitti.piyapan@gmail.com>
2018-10-10 19:47:15 +0700

    add route and css

---
+++ src/HomeRoute.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export class HomeRoute extends React.Component {
+  render () {
+    return (
+      <div>
+        <div className="card-profile--container">
+          <div className="card card-profile">
+            <img className="profile-avatar" src="https://www.biography.com/.image/t_share/MTQ3NjM5MTEzMTc5MjEwODI2/eminem_photo_by_dave_j_hogan_getty_images_entertainment_getty_187596325.jpg" />
+            <div className="profile-name">Ti Teller</div>
+            <div className="profile-desc">Wtf</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter, Switch, Route, Link } from 'react-router-dom'
+import { HomeRoute } from './HomeRoute';
 
-ReactDOM.render(<div>555 wp</div>, document.getElementById('root'))
+const element = (
+  <HashRouter>
+    <div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={HomeRoute}
+        />
+        <Route
+          render={() => (
+            <div>
+              <h1>404</h1>
+              <Link to="/">Go to home...</Link>
+            </div>
+          )}
+        />
+      </Switch>
+    </div>
+  </HashRouter>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
